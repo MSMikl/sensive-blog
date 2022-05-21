5df03e731b0427c9ac833784d857cc872769e5e2
MSMikl <michalbl4@gmail.com>
2022-05-21 20:20:52 +0300

    reduce db queries number

--- blog/views.py
+++ blog/views.py
@@ -31,7 +31,12 @@ def serialize_post_optimized(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [
+            serialize_tag_optimized(tag) for tag in (
+                post.tags.
+                annotate(posts_count=Count('posts'))
+            )
+        ],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -40,6 +45,13 @@ def serialize_tag(tag):
     return {
         'title': tag.title,
         'posts_with_tag': len(Post.objects.filter(tags=tag)),
+    }
+
+
+def serialize_tag_optimized(tag):
+    return {
+        'title': tag.title,
+        'posts_with_tag': tag.posts_count,
     }
 
 
@@ -58,7 +70,10 @@ def index(request):
         prefetch_related('author', 'tags')
     )
 
-    most_popular_tags = Tag.objects.popular()[:5]
+    most_popular_tags = (
+        Tag.objects.
+        popular()[:5]
+    )
 
     context = {
         'most_popular_posts': [
@@ -67,7 +82,9 @@ def index(request):
         'page_posts': [
             serialize_post_optimized(post) for post in most_fresh_posts
         ],
-        'popular_tags': [serialize_tag(tag) for tag in most_popular_tags],
+        'popular_tags': [
+            serialize_tag_optimized(tag) for tag in most_popular_tags
+        ],
     }
     return render(request, 'index.html', context)
 

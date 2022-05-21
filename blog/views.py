d22a78d03bebaf22d4a2f0fee27631662f3c22a5
MSMikl <michalbl4@gmail.com>
2022-05-21 15:28:14 +0300

    move code to reusable QuerySet method

--- blog/views.py
+++ blog/views.py
@@ -64,9 +64,7 @@ def index(request):
         annotate(comments_count=Count('comments'))
     )
 
-    most_popular_tags = Tag.objects.annotate(
-        posts_count=Count('posts')
-    ).order_by('-posts_count')[:5]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     context = {
         'most_popular_posts': [
@@ -111,9 +109,7 @@ def post_detail(request, slug):
         'tags': [serialize_tag(tag) for tag in related_tags],
     }
 
-    most_popular_tags = Tag.objects.annotate(
-        posts_count=Count('posts')
-    ).order_by('-posts_count')[:5]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = Post.objects.annotate(
         likes_count=Count('likes')
@@ -132,9 +128,7 @@ def post_detail(request, slug):
 def tag_filter(request, tag_title):
     tag = Tag.objects.get(title=tag_title)
 
-    all_tags = Tag.objects.all()
-    popular_tags = sorted(all_tags, key=get_related_posts_count)
-    most_popular_tags = popular_tags[-5:]
+    most_popular_tags = Tag.objects.popular()[:5]
 
     most_popular_posts = []  # TODO. Как это посчитать?
 
